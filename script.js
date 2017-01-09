b88d9798c6c7b9a903dede4687519879a8f325f4
vverovvero <wendy.w.chen@yale.edu>
2017-01-08 18:39:38 -0800

    Modified nav bar color.  Added Design gallery.  9/10

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function myFunction() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         navbar.className = "w3-navbar" + " w3-card-2" + " w3-animate-top" + " w3-white";
     } else {
-        navbar.className = navbar.className.replace(" w3-card-2 w3-animate-top w3-white", "");
+        navbar.className = navbar.className.replace(" w3-card-2 w3-animate-top w3-white", " w3-text-white");
     }
 }
 
